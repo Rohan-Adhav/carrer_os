c625b199bbb91084338d1f024f88e4436e20610d
Rohan Adhav <rohanadhav78@gmail.com>
2026-06-09 14:19:53 +0530

    feat: add health check endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("Career OS API")
+    res.send("Career OS API");
 });
+
+app.get("/health",(req,res)=>{
+    res.send("OK");
+});
+
 export default app;
